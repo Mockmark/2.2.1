1f4f8ebe4ae6ed7deead2cfd5ca70bf02196fe71
Michael Mark <selfdevourment@gmail.com>
2025-04-04 00:05:58 +0500

    Realize through cascade.

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,34 +1,57 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        Car car1 = new Car("a", 1);
+        Car car2 = new Car("b", 2);
+        Car car3 = new Car("c", 3);
+        Car car4 = new Car("c", 3);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+        User user1 = new User("User1", "User1", "user1@hiber.com", car1);
+        User user2 = new User("User2", "User2", "user2@hiber.com", car2);
+        User user3 = new User("User3", "User3", "user3@hiber.com", car3);
+        User user4 = new User("User4", "User4", "user4@hiber.com", car4);
+        user4.setCar(car1);
+        user4.setCar(car4);
 
-      context.close();
-   }
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
+        userService.add(user4);
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car = " + user.getCar().toString());
+            System.out.println();
+        }
+
+        List<User> usersSpecific = userService.getUsersByCar("c", 3);
+        for (User user : usersSpecific) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car = " + user.getCar().toString());
+            System.out.println();
+        }
+
+        context.close();
+    }
 }
---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,56 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long carId;
+
+    @Column
+    private String model;
+
+    @Column
+    private int series;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public void setCarId(Long carId) {
+        this.carId = carId;
+    }
+
+    public Long getCarId() {
+        return carId;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    @Override
+    public String toString() {
+        return "carId: " + carId + ", model: "
+                + model + ", series: " + series;
+    }
+}
